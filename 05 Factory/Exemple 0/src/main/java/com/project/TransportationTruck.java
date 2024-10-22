06214344a5e1f3408001a9391384868346da2eb0
optimisme <optimisme@gmail.com>
2024-10-22 15:37:34 +0200

    .

--- 05 Factory/Exemple 0/src/main/java/com/project/TransportationTruck.java
+++
@@ -1,9 +0,0 @@
-package com.project;
-
-public class TransportationTruck implements Transportation {
-
-    @Override
-    public void deliverPackage() {
-        System.out.println("Highway traveling");
-    }
-}
--- 05 Factory/Exemple 0/src/main/java/com/project/TransportationVan.java
+++
@@ -1,9 +0,0 @@
-package com.project;
-
-public class TransportationVan implements Transportation {
-
-    @Override
-    public void deliverPackage() {
-        System.out.println("City delivery");        
-    }
-}
