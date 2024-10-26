65399e18b8f70226e32029cf4be478bc9fdb21c7
optimisme <optimisme@gmail.com>
2024-10-26 13:59:27 +0200

    .

---
+++ 06 Classificació de text/Exemple Y/ai_train.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+import os
+import json
+print("Loading AI libraries ..."); 
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from transformers import BertTokenizer
+from typing import List, Union
+from ai_utils_text import ModelConfig, ModelDataset, ModelClassifier, EarlyStopping, getDevice
+
+CONFIG_FILE = "model_config.json"
+
+def clearScreen():
+    if os.name == 'nt':     # Si estàs a Windows
+        os.system('cls')
+    else:                   # Si estàs a Linux o macOS
+        os.system('clear')
+
+clearScreen()
+
+from tqdm import tqdm
+
+def train_epoch(model, dataloader, optimizer, criterion, device):
+    model.train()
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    # Afegeix una barra de progrés
+    for batch in tqdm(dataloader, desc="Training", leave=True, bar_format="{desc}:   {percentage:3.2f}% |{bar:20}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]"):
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['label'].to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(input_ids, attention_mask)
+        loss = criterion(outputs, labels)
+        
+        loss.backward()
+        optimizer.step()
+        
+        total_loss += loss.item()
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+    
+    accuracy = correct / total
+    return total_loss / len(dataloader), accuracy
+
+def evaluate_epoch(model, dataloader, criterion, device):
+    model.eval()
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        # Afegeix una barra de progrés
+        for batch in tqdm(dataloader, desc="Evaluating", leave=True, bar_format="{desc}: {percentage:3.2f}% |{bar:20}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]"):
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['label'].to(device)
+            
+            outputs = model(input_ids, attention_mask)
+            loss = criterion(outputs, labels)
+            
+            total_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    
+    accuracy = correct / total
+    return total_loss / len(dataloader), accuracy
+
+
+def main():
+
+    # Carregar la configuració
+    with open(CONFIG_FILE) as f:
+        config_file = json.load(f)
+
+    # Carregar i preprocessar les dades
+    print("Loading data ...")
+    df = pd.read_csv(config_file['paths']['data'], encoding='utf-8')
+    df = df[['label', 'text']]
+    df = df.dropna()
+
+    labels = df['label'].unique().tolist()
+    print("Labels:", labels)
+
+    # Configuració del model
+    config = ModelConfig(config_file, labels)
+    
+    le = LabelEncoder()
+    df['label'] = le.fit_transform(df['label'].astype(str))
+
+    print("Initialize tokenizer...")
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    VOCAB_SIZE = tokenizer.vocab_size
+
+    # Dividir les dades
+    X_train, X_test, y_train, y_test = train_test_split(
+        df['text'], df['label'], test_size=0.2, random_state=42
+    )
+
+    # Crear datasets i dataloaders
+    train_dataset = ModelDataset(X_train.values, y_train.values, tokenizer, config.max_len)
+    test_dataset = ModelDataset(X_test.values, y_test.values, tokenizer, config.max_len)
+
+    train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=config.batch_size)
+
+    # Inicialitzar model i components d'entrenament
+    device = getDevice()
+
+    model = ModelClassifier(config).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+    criterion = nn.CrossEntropyLoss()
+    early_stopping = EarlyStopping(patience=config.patience)
+
+    # Entrenament
+    best_accuracy = 0.0  # Per emmagatzemar la millor precisió observada
+    for epoch in range(config.epochs):
+        
+        print("")
+        
+        train_loss, train_acc = train_epoch(model, train_loader, optimizer, criterion, device)
+        test_loss, test_acc = evaluate_epoch(model, test_loader, criterion, device)
+        
+        print(f'Epoch {epoch+1}/{config.epochs} (Train: loss: {train_loss:.2f}, accuracy: {train_acc:.2f}) (Eval: loss: {test_loss:.2f}, accuracy: {test_acc:.2f})')
+        
+        if test_acc > best_accuracy:
+            best_accuracy = test_acc
+            torch.save(model.state_dict(), config_file['paths']['trained_network'])
+            print(f"New best model saved with accuracy {best_accuracy:.2f}")
+
+        # Guarda les metadades
+        metadata = { "unique_categories": labels, "label_encoder": le.classes_.tolist() }
+        with open(config_file['paths']['metadata'], 'w') as metadata_file:
+            json.dump(metadata, metadata_file)
+
+        # Comprova l'early stopping
+        early_stopping(test_loss, model)
+        if early_stopping.early_stop:
+            print("Early stopping triggered")
+            model.load_state_dict(early_stopping.get_best_model())
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ 06 Classificació de text/Exemple Y/ai_utils_text.py
@@ -0,0 +1,146 @@
+import os
+import json
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from transformers import BertTokenizer
+from typing import List, Union
+
+class ModelConfig:
+    def __init__(self, config_file, labels):
+        # Configuració general del model
+        self.is_binary = config_file['model_configuration']['is_binary']
+        self.class_labels = labels or ['ham', 'spam']
+        self.num_classes = 2 if self.is_binary else len(self.class_labels)
+
+        # Configuració de camins
+        self.data_path = config_file['paths']['data']
+        self.trained_network_path = config_file['paths']['trained_network']
+        self.metadata_path = config_file['paths']['metadata']
+
+        # Configuració de columnes
+        self.category_column = config_file['columns']['categories']
+        self.text_column = config_file['columns']['text']
+
+        # Configuració del model i l'optimitzador
+        self.batch_size = config_file['training']['batch_size']
+        self.epochs = config_file['training']['epochs']
+        self.learning_rate = config_file['training']['learning_rate']
+        self.max_len = config_file['model_configuration']['max_len']
+        
+        # Configuració de capes dinàmiques
+        self.layers = config_file['model_configuration']['layers']
+        
+        # Configuració d'early stopping
+        self.patience = config_file['optimization']['early_stopping']['patience']
+        self.min_delta = config_file['optimization']['early_stopping']['min_delta']
+
+# Classes del model
+class ModelDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_len):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, idx):
+        text = str(self.texts[idx])
+        label = self.labels[idx]
+        
+        encoding = self.tokenizer(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt'
+        )
+        
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'label': torch.tensor(label, dtype=torch.long)
+        }
+
+class ModelClassifier(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super(ModelClassifier, self).__init__()
+        self.config = config
+        layers = []
+
+        for layer_config in config.layers:
+            layer_type = layer_config['type']
+            
+            if layer_type == 'Embedding':
+                layers.append(nn.Embedding(
+                    num_embeddings=layer_config['vocab_size'],
+                    embedding_dim=layer_config['embedding_dim']
+                ))
+            elif layer_type == 'Dropout':
+                layers.append(nn.Dropout(p=layer_config['p']))
+            elif layer_type == 'Linear':
+                num_out_features = 0
+                if layer_config['out_features'] == "unique_categories":
+                    num_out_features = config.num_classes
+                else:
+                    num_out_features = layer_config['out_features']
+
+                layers.append(nn.Linear(
+                    in_features=layer_config['in_features'],
+                    out_features=num_out_features
+                ))
+            elif layer_type == 'ReLU':
+                layers.append(nn.ReLU())
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, input_ids, attention_mask):
+        x = self.network[0](input_ids)
+        masked = x * attention_mask.unsqueeze(-1)
+        x = masked.mean(dim=1)
+        
+        for layer in self.network[1:]:
+            x = layer(x)
+
+        return x
+
+class EarlyStopping:
+    def __init__(self, patience: int = 3, min_delta: float = 0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+        self.best_model = None
+        
+    def __call__(self, val_loss: float, model: nn.Module):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+            self.best_model = model.state_dict().copy()
+        elif val_loss > self.best_loss - self.min_delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_loss = val_loss
+            self.best_model = model.state_dict().copy()
+            self.counter = 0
+            
+    def get_best_model(self):
+        return self.best_model
+    
+def getDevice():
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
+    if device.type == "cuda" or device.type == "mps":
+        print(f"Using device: {device} (GPU accelerated)")
+    else:
+        print(f"Using device: {device} (CPU based)")
+    
+    return device
